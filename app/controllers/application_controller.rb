5639581a6c2193f8f29368a6b1c0f5e5ac359e29
Takashi <Takashi@gmail.com>
2019-09-17 19:34:41 +0900

    ユーザー詳細画面を作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,12 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nick_name])
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:icon])
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:icon_cache])
+    user_attrs = [
+      :name, :nick_name, :icon, :icon_cache,
+    ]
+    devise_parameter_sanitizer.permit(:sign_up, keys: user_attrs)
+    devise_parameter_sanitizer.permit(:account_update, keys: user_attrs)
   end
 end
