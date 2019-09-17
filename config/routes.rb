5a98d529008fe2dca67bbf0435656db9a529df71
Takashi <Takashi@gmail.com>
2019-09-17 21:33:46 +0900

    Movie投稿時に画像を投稿できるようにした

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,7 @@
 Rails.application.routes.draw do
-  get 'movies/index'
-  get 'movies/new'
-  get 'movies/show'
-  get 'movies/edit'
-  get 'movies/create'
-  get 'movies/update'
-  get 'movies/destroy'
-  get 'reviews/index'
-  get 'reviews/new'
-  get 'reviews/show'
-  get 'reviews/edit'
-  get 'reviews/create'
-  get 'reviews/update'
-  get 'reviews/destroy'
   root 'homes#index'
+  resources :movies
+  resources :reviews
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
