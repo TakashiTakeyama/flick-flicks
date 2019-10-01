651804360b1b1da2eb3225fc613940568a872038
Takashi <Takashi@gmail.com>
2019-10-01 13:38:53 +0900

    cssを修正

---
+++ app/assets/javascripts/custum.js
@@ -0,0 +1,7 @@
+let mySwiper = new Swiper ('.swiper-container', {
+    effect: "flip",
+    loop: true,
+    pagination: '.swiper-pagination',
+    nextButton: '.swiper-button-next',
+    prevButton: '.swiper-button-prev',
+})
