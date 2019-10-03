df1f7173758ed96951f3cc34d5df0e2df5ec34ff
Takashi <Takashi@gmail.com>
2019-10-03 14:28:26 +0900

    デザインの修正

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,5 @@
 class Review < ApplicationRecord
-  validates :impression, presence: true, length: { maximum: 300 }
+  validates :impression, presence: true, length: { maximum: 1000 }
   has_many :favorite_reviews, dependent: :destroy
   # has_many :favorite_users, through: :favorite_reviews, source: :user
   belongs_to :user, optional: true
