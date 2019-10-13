3024795a4243f39efa4975663fd51899b0b520b0
Takashi <Takashi@gmail.com>
2019-10-13 17:03:27 +0900

    おすすめ動画機能を修正

--- app/assets/javascripts/custum.js
+++ app/assets/javascripts/custum.js
@@ -1,21 +1,29 @@
+// var mySwiper = new Swiper ('.swiper-container', {
+//     // Optional parameters
+//     direction: 'vertical',
+//     loop: true,
+//
+//     // If we need pagination
+//     pagination: {
+//         el: '.swiper-pagination',
+//     },
+//
+//     // Navigation arrows
+//     navigation: {
+//         nextEl: '.swiper-button-next',
+//         prevEl: '.swiper-button-prev',
+//     },
+//
+//     // And if we need scrollbar
+//     scrollbar: {
+//         el: '.swiper-scrollbar',
+//     },
+// })
+
 var mySwiper = new Swiper ('.swiper-container', {
-    // Optional parameters
-    direction: 'vertical',
+    effect: "flip",
     loop: true,
-
-    // If we need pagination
-    pagination: {
-        el: '.swiper-pagination',
-    },
-
-    // Navigation arrows
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-
-    // And if we need scrollbar
-    scrollbar: {
-        el: '.swiper-scrollbar',
-    },
+    pagination: '.swiper-pagination',
+    nextButton: '.swiper-button-next',
+    prevButton: '.swiper-button-prev'
 })
