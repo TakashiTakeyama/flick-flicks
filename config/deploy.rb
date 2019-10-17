7905f6eb9df60dfb5f65cd0cc5370204e237281a
Takashi <Takashi@gmail.com>
2019-10-17 14:59:12 +0900

    デプロイの為の準備

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :repo_url, 'https://github.com/TakashiTakeyama/flick-flicks'
 set :branch, ENV['BRANCH'] || 'master'
 
 # deploy先のディレクトリ。
-set :deploy_to, '/var/www/achieve'
+set :deploy_to, '/var/www/flick-flicks'
 
 # シンボリックリンクをはるフォルダ・ファイル
 set :linked_files, %w{.env config/secrets.yml}
